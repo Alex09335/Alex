31adb498918e91bb81df9ad20807c060b5dbc811
AF <m093.rahaei@gmail.com>
2024-04-07 17:52:57 +0330

    add python

--- practice.py
+++ practice.py
@@ -1,4 +1,15 @@
-firens=["ali","reza","arash","sara"]
+users={"ali":1234,
+       "reza":5432,
+       "ashkan":3245}
 
-for i in firens:
-    print(firens)
+
+while True:
+    enter_user=input("enter your username:")
+    enter_password=int(input("enter your password:"))
+    if enter_user in users and enter_password==users[enter_user]:
+        print("you log successfuly")
+        break
+    else:
+        print("your username or password is wrong")
+
+    
