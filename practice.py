ae98d8227a1ca58f409b4536040f0ec5358d7a59
AF <m093.rahaei@gmail.com>
2024-04-15 15:53:01 +0330

    add python

--- practice.py
+++ practice.py
@@ -1,15 +1,17 @@
-users={"ali":1234,
-       "reza":5432,
-       "ashkan":3245}
+username=input("enter your fullname:")
 
+for n in username:
+    c=f"your name has:{username.count(n)} {n} "
+    print(c)
 
-while True:
-    enter_user=input("enter your username:")
-    enter_password=int(input("enter your password:"))
-    if enter_user in users and enter_password==users[enter_user]:
-        print("you log successfuly")
-        break
-    else:
-        print("your username or password is wrong")
 
     
+    
+    
+   
+    
+        
+    
+   
+    
+     
