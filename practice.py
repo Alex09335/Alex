204c7ded8d5a858d5cf1b8084f06257c5a60b603
AF <m093.rahaei@gmail.com>
2024-04-20 18:28:44 +0330

    add python

--- practice.py
+++ practice.py
@@ -1,24 +1,16 @@
-username=input("enter your fullname:")
-username=username.replace(" ","")
-
-b=[]
-for n in username:
-    if n not in b:
-        
-        c=f"your name has:{username.count(n)} {n} "
-        print(c)
-        b.append(n)
-        
+def password_valid(password):
+    if password_user.isalpha():
+        print("your password must have at least 1 number")
+    elif len(password_user)<8:
+        print("your password must be 8 number and charechtor") 
+    elif password_user.isnumeric():
+        print("your password must have at least 1 charechtor")
+    else:
+        print("your password is correct")
 
 
-
+while True:
+    password_user=input("enter your password:")
     
     
-    
-   
-    
-        
-    
-   
-    
-     
+    password_valid(password_user)
