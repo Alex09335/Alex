91cd53448eab6991332a41087213f6a412e0092a
AF <m093.rahaei@gmail.com>
2024-05-01 20:19:26 +0330

    add

--- practice.py
+++ practice.py
@@ -1,16 +1,19 @@
-def password_valid(password):
-    if password_user.isalpha():
-        print("your password must have at least 1 number")
-    elif len(password_user)<8:
-        print("your password must be 8 number and charechtor") 
-    elif password_user.isnumeric():
-        print("your password must have at least 1 charechtor")
-    else:
-        print("your password is correct")
+name=input("enter your name :")
 
-
-while True:
-    password_user=input("enter your password:")
+def myfunction(name):
+    ups=0
+    lows=0
+    for n in name:
+        if n.isupper():
+            ups=ups+1
+        
+        elif n.islower():
+            lows=lows+1
+           
+        else:
+            pass
+    print(f"upper case is  {ups}")
+    print(f"lows case is {lows}")
     
-    
-    password_valid(password_user)
+myfunction(name)        
+     
