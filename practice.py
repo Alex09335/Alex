a2dae14ee2377ce07ad00e637b31e0ec8dd92f8f
AF <m093.rahaei@gmail.com>
2024-05-27 15:38:44 +0330

    add lambda

--- practice.py
+++ practice.py
@@ -1,18 +1,3 @@
-day_user=int(input("enter the day:"))
-month_user=int(input("enter the month:"))
-year_user=int(input("enter the year:"))
-def converter(day_user,month_user,year_user):
-    if month_user==10 and day_user>10:
-        intendedyear=year_user+622
-      
-    elif month_user>10:
-        intendedyear=year_user+622
-      
-    else:
-        intendedyear=year_user+621
-                   
-        print(intendedyear)   
-          
-converter(day_user,month_user,year_user)
-        
-        
+x=lambda a,b,c:a+b+c
+print(x(10,20,30))
+
