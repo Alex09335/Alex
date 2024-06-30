0437c822c52473bc9f41a13bed611fc61e29b7b2
AF <m093.rahaei@gmail.com>
2024-06-30 15:10:36 +0330

    add python file

--- practice.py
+++ practice.py
@@ -1,3 +1,20 @@
-x=lambda a,b,c:a+b+c
-print(x(10,20,30))
+firstnumber=int(input("enter your first number:"))
+secondnumber=int(input("enter your second number:"))
+thirdnumber=int(input("enter your third number:"))
 
+if firstnumber >secondnumber and secondnumber>thirdnumber:
+    print(firstnumber,secondnumber,thirdnumber)
+    
+elif secondnumber>firstnumber and firstnumber>thirdnumber:
+    print(secondnumber,firstnumber,thirdnumber)
+elif secondnumber>firstnumber and thirdnumber>firstnumber:
+    print(secondnumber,thirdnumber,firstnumber)
+    
+elif thirdnumber>firstnumber and firstnumber>secondnumber:
+    print(thirdnumber,firstnumber,secondnumber)
+elif thirdnumber>firstnumber and secondnumber>firstnumber:
+    print(thirdnumber,secondnumber,firstnumber)    
+else:
+    print(" ")
+    
+              
